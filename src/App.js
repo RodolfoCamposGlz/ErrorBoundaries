6160fd67971d024ea41a4ccb2c768abb2d2f8a12
RodolfoCamposGlz <rodolfocampos459@gmail.com>
2019-12-18 18:09:40 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hero from './Hero';
+import ErrorBoundary from './ErrorBoundary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <ErrorBoundary>
+          <Hero heroName="Batman"></Hero>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Superman"></Hero>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Joker"></Hero>
+        </ErrorBoundary>
+      </div>
+    )
+  }
 }
-
 export default App;
